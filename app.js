0214ac7728025bef29f0753ae880b774c6337895
Hennadii Kupriiuk <hennadii.kupriiuk@gmail.com>
2026-03-02 22:06:50 +0200

    setup basic server

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+require('express-async-errors');
+
+// express
+const express = require('express');
+const app = express();
+
+// rest of the packages
+const morgan = require('morgan');
+
+// database
+const connectDB = require('./db/connect');
+
+// middleware
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('E-Commerce API')
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 1234;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(port || 1234, () => {
+      console.log(`Server is listening on port ${port}`)
+    });
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+start();
