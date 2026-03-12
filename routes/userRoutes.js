12a5f36ff194cf33f70e3de92f6623582c0b404f
Hennadii Kupriiuk <hennadii.kupriiuk@gmail.com>
2026-03-12 22:15:51 +0200

    add another routes, minor fixes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPassword } = require('../controllers/userController');
+const { getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPassword } = require('../controller/userController');
 
 router.route('/').get(getAllUsers);
+
+router.route('/showMe').get(showCurrentUser);
+
+router.route('/updateUser').patch(updateUser);
+router.route('/updateUserPassword').patch(updateUserPassword);
+
+router.route(':id').get(getSingleUser);
+
+module.exports = router;
