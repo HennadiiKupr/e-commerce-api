7d7c449385a428903d3095cd72d0a51928a9fc28
Hennadii Kupriiuk <hennadii.kupriiuk@gmail.com>
2026-03-13 23:54:03 +0200

    Add UnauthorizedError class and enhance authentication middleware with role-based access control

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
 
 const { getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPassword } = require('../controller/userController');
 
-router.route('/').get(getAllUsers);
+router.route('/').get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers);
 
 router.route('/showMe').get(showCurrentUser);
 
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
 
-router.route('/:id').get(getSingleUser);
+router.route('/:id').get(authenticateUser, getSingleUser);
 
 module.exports = router;
