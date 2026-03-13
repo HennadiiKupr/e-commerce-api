b2cf4cb8fc1e2fd126b88f69962f76ffac6aad28
Hennadii Kupriiuk <hennadii.kupriiuk@gmail.com>
2026-03-13 23:59:39 +0200

    Enhance showCurrentUser route with authentication middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const { getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPassw
 
 router.route('/').get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers);
 
-router.route('/showMe').get(showCurrentUser);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
 
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
