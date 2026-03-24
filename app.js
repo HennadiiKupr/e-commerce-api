6bc2a902b6005af4576256c4e65d772d0f89b416
Hennadii Kupriiuk <hennadii.kupriiuk@gmail.com>
2026-03-24 21:38:00 +0200

    Add review model, controller, and routes; implement CRUD operations for reviews

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const notFoundMiddleware = require('./middleware/not-found');
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
 
 app.use(morgan('tiny'));
 app.use(express.json());
@@ -37,6 +38,7 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
