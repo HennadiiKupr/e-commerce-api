82864848add9f9ef34125d02f4f0842c3acd1e9d
Hennadii Kupriiuk <hennadii.kupriiuk@gmail.com>
2026-03-29 21:50:29 +0300

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ const app = express();
 // rest of the packages
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 // database
 const connectDB = require('./db/connect');
@@ -25,12 +30,22 @@ const productRouter = require('./routes/productRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const orderRouter = require('./routes/orderRoutes');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+}));
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static('./public'));
 app.use(fileUpload());
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('E-Commerce API')
